3e6f3698911ca3fd1b9ff049d9f3f86867c2dfb5
kawabata_shintaro <4499135+kawabatas@users.noreply.github.com>
2019-03-28 16:12:53 +0900

    temp

--- echo.go
+++ echo.go
@@ -224,7 +224,7 @@ const (
 
 const (
 	// Version of Echo
-	Version = "4.0.0-kawabatas"
+	Version = "4.0.1"
 	website = "https://echo.labstack.com"
 	// http://patorjk.com/software/taag/#p=display&f=Small%20Slant&t=Echo
 	banner = `
