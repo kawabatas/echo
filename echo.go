bc0900c84416fb23a1e3df88f74f14cd622f7c0f
kawabata_shintaro <4499135+kawabatas@users.noreply.github.com>
2019-03-28 15:50:11 +0900

    fix version

--- echo.go
+++ echo.go
@@ -224,7 +224,7 @@ const (
 
 const (
 	// Version of Echo
-	Version = "4.0.0-kawabata"
+	Version = "4.0.0-kawabatas"
 	website = "https://echo.labstack.com"
 	// http://patorjk.com/software/taag/#p=display&f=Small%20Slant&t=Echo
 	banner = `
